443ef5811173ecc73bb6de1c08f43a5c6f2fa489
mreiley <sdkmarior@gmail.com>
2023-01-13 10:46:52 -0400

    Finish

--- server.go
+++ server.go
@@ -1,3 +1,11 @@
+/*
+AUTOR: Mario Reiley backend Developer
+NOTE: it is my port to Golang 1.19
+
+* You can submit a form that includes a file upload.
+* The form file input field has the name attribute set to upfile.
+* When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
+*/
 package main
 
 import (
@@ -7,13 +15,11 @@ import (
 	"os"
 )
 
-// Project json format
-
 type jerr struct {
 	Msg string `json:"msj"`
 }
 
-// {"name":"Mario20Dollares.pdf","type":"application/pdf","size":7156}:q
+// Create mehtos for this struct
 type meta struct {
 	Name string `json:"name"`
 	Type string `json:"type"`
@@ -33,12 +39,13 @@ func HandlerSendFile(w http.ResponseWriter, req *http.Request) {
 	return
 }
 
+// I did test with postman and Thunder client vscode extension and work fime
 func fileanalyse(w http.ResponseWriter, req *http.Request) {
 
 	_, fh, e := req.FormFile("upfile")
 
 	if e != nil {
-		log.Println(e)
+		log.Println(e) // Just log the error and continue
 	} else {
 
 		res, _ := json.Marshal(meta{Name: fh.Filename, Type: fh.Header.Get("Content-Type"), Size: fh.Size})
